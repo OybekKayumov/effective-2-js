af13f04fad5cc82694ee0b7957363e5714da3683
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-02 20:38:42 +0500

    add slick-carousel

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import styled from 'styled-components';
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function ImgSlider() {
+
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  }
+
   return (
-    <div>ImgSlider</div>
+    <Carousel {...settings}>
+        <Wrap>
+          <img src='/images/slider-badging.jpg' alt='' />
+        </Wrap>
+    </Carousel>
   )
 }
 
 export default ImgSlider
+
+const Carousel = styled(Slider)`
+
+`
+
+const Wrap = styled.div`
+
+`
