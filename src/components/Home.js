d6976fd62896f5a42fad7c5f6db690261ccbe98d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-02 20:12:07 +0500

    add ImgSlider to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
 
 function Home() {
   return (
     <Container>
-      Home
+      <ImgSlider />
     </Container>
   )
 }
