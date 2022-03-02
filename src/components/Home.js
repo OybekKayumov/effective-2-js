f64f0b068871d4d631cc07c3e098cee2d8518038
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-02 21:18:31 +0500

    update carousel git add .!

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,6 +16,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px  );
   padding: 0 calc(3.5vw + 5px);
   position: relative;
+  overflow-x: hidden;
 
   &::before {
     background: url("/images/home-background.png") center center  / cover no-repeat fixed;
@@ -26,6 +27,5 @@ const Container = styled.main`
     right: 0;
     bottom: 0;
     z-index: -1;
-
   }
 `
